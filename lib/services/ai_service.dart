4cea0047621e7003f1a278cb7def48ecb43a9395
Prankur Sharma <prankur@example.com>
2026-04-28 23:33:25 +0530

    chore: move API key to ignored config file and reference it in AIService

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:ai_finance_manager/config/api_keys.dart';
 
 class AIService {
-  static const _apiKey = 'AIzaSyBcT0rbB7kfGXDC09IdAQ1vJ_28w7TOwpU';
+  static const _apiKey = ApiKeys.geminiApiKey;
 
   static Future<String> getSmartTips() async {
     final user = FirebaseAuth.instance.currentUser;
